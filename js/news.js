46d02313b7129d60780c7cb84174e541f737aed8
abdulhakim10 <s.abdulhakim10@gmail.com>
2022-09-03 12:22:04 +0600

    categories section is added

--- js/news.js
+++ js/news.js
@@ -1,1 +1,19 @@
-console.log('added')
+const loadNewsCategories = () => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories'
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsCategories(data.data.news_category))
+}
+
+const displayNewsCategories = categories => {
+    const newsCategories = document.getElementById('news-categories');
+    categories.forEach(category => {
+        // console.log(category)
+        const categoryDiv = document.createElement('div')
+        categoryDiv.innerHTML = `
+        <p>${category.category_name}</p>
+        `;
+        newsCategories.appendChild(categoryDiv);
+    })
+}
+loadNewsCategories()
